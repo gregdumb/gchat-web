30cb84c9d1c93c55c8b2b79e4e6efbf00e555dd7
Greg <gregoryjjb@gmail.com>
2018-05-02 15:37:24 -0700

    Added API class

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const apiURL = '/api/';
+
+const api = {};
+
+api.sendRegistration = ({firstName, lastName, email, password}) => {
+    axios.post(
+        `${apiURL}register`,
+        {
+            firstName,
+            lastName,
+            email,
+            password
+        }
+    )
+    .then(() => {
+
+    })
+    .catch(() => {
+
+    })
+}
