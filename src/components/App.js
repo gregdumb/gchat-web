f7a648ce5c57c7127ff12f76ff96d56a351110b1
Greg Brisebois <gregoryjjb@gmail.com>
2018-05-06 10:06:59 -0700

    Added validation

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,37 @@ const form = [{
 	name: 'firstName',
 	label: 'First name',
 	type: 'text',
-	defaultValue: 'abcdefg',
-}]
+	defaultValue: 'greg',
+	required: true,
+},{
+	name: 'lastName',
+	label: 'Last name',
+	type: 'text',
+	defaultValue: 'breezy',
+	validation: s => s.length > 3,
+},{
+	name: 'checkTest',
+	label: 'Check test',
+	type: 'checkbox',
+	defaultValue: true,
+},{
+	name: 'selectTest',
+	label: 'Select test',
+	type: 'select',
+	options: [{value: 'pick-me', display: 'Pick me'}, {value: 'or-me', display: 'Or me'}],
+	defaultValue: 'or-me',
+},{
+	name: 'dateTest',
+	label: 'Date test',
+	type: 'date',
+} ]
 
 const App = withStore(({ store }) => 
 	<div>
 		<h1>gChat</h1>
 		{store.get('sessionKey') === '' ?
 			<div>
-				<Form fields={form} />
+				<Form fields={form} onSubmit={(v) => console.log("Submitted", v)} />
 			</div>
 		:
 			<div>
--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
-const Input = ({ field, value, onChange }) => {
+const Input = ({ field, value, valid, onChange, onBlur }) => {
+	
+	let input;
+	let style = {color: valid ? 'unset' : 'red' }
 	
 	if(field.type === 'select') {
 		
+		input = (
+			<select
+				name={field.name}
+				onChange={onChange}
+				onBlur={onBlur}
+			>
+				{field.options.map(o => (
+					<option value={o.value} key={o.value} selected={o.value === field.defaultValue} >{o.display}</option>
+				))}
+			</select>
+		)
 	}
 	else {
-		return (
+		input = (
 			<input 
 				name={field.name}
+				type={field.type}
 				value={value}
 				onChange={onChange}
+				onBlur={onBlur}
 			/>
 		)
 	}
 	
+	return(
+		<label style={style} >
+			{field.label}
+			{input}
+		</label>
+	)
 }
 
 export default Input;
